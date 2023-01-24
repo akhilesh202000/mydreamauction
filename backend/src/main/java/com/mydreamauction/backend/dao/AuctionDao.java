a3b1f518ce06e430b41b1c8029884d65126d8877
Akhilesh Singh <akhilesh202000@gmail.com>
2023-01-24 10:39:57 +0100

    delete everything

--- backend/src/main/java/com/mydreamauction/backend/dao/AuctionDao.java
+++
@@ -1,8 +0,0 @@
-package com.mydreamauction.backend.dao;
-
-import com.mydreamauction.backend.model.DAOAuction;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuctionDao extends CrudRepository<DAOAuction, Integer> {
-
-}
--- backend/src/main/java/com/mydreamauction/backend/dao/ProductDao.java
+++
@@ -1,7 +0,0 @@
-package com.mydreamauction.backend.dao;
-
-import com.mydreamauction.backend.model.DAOProduct;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductDao extends CrudRepository<DAOProduct, Integer> {
-}
